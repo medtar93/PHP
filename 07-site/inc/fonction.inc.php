f12a11a750d61023a7b2ff75b5d8906427ab5c33
Mohammed-Tarek Benkherouf <39194157+medtar93@users.noreply.github.com>
2018-08-22 17:04:25 +0200

    c12

---
+++ 07-site/inc/fonction.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+
+//---------------------fonction de debug----------------
+
+function debug($param) {
+    echo '<pre>';
+        print_r($param);
+        echo '</pre>';
+}
+
+//-------------------fonctions membres------------------
+
+//Fonction qui indique si l'internaute est connécté :
+function internauteEstConnecte() {
+    
+    if (isset($_SESSION['membre'])){ //si la session "membre existe, c'est que l'internaute est passé par la page de connexion et que nous avons créé cet indice dans $_SESSION
+
+
+    return true;
+} else {
+    return false;     
+}
